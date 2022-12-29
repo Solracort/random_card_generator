0d6ff8c42ad0c457717a28ce3423852563967f3b
Solracort <solracort@gmail.com>
2022-12-29 20:38:13 +0000

    errores no localizados

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
+const nombrecarta = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let indicepalo = Math.floor(Math.random() * 4);
+  let indicecarta = Math.floor(Math.random() * 13);
+  let numcarta = document.querySelector(".numero");
+  numcarta.innerHTML = nombrecarta[indicecarta];
+  let paloHTML = document.querySelector(".palo");
+  switch (indicepalo) {
+    case 0:
+      paloHTML.innerHTML = "<img src='/src/assets/img/hearts.png'/>";
+      break;
+    case 1:
+      paloHTML.innerHTML = "<img src='/src/assets/img/spades.png'/>";
+      break;
+    case 2:
+      paloHTML.innerHTML = "<img src='/src/assets/img/clubs.png'/>";
+      break;
+    case 3:
+      paloHTML.innerHTML = "<img src='/src/assets/img/hearts.png'/>";
+      break;
+    default:
+      console.log("default");
+  }
 };
